66bda2409f3d2a4ff0e7d9111cc558622a8311df
kota.ueda <kota.ueda@keenova.co.jp>
2025-12-05 12:20:41 +0900

    Chapter10のサンプルコード

--- src_ref/app/Http/Controllers/Api/ArticleController.php
+++ src_ref/app/Http/Controllers/Api/ArticleController.php
@@ -8,15 +8,20 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
+    // 記事一覧の取得
     public function index(Request $request)
     {
         $page = $request->query('page', 1);
 
         if (!is_numeric($page) || $page < 1) {
-            return response("Invalid parameter.", 400);
+            return response()->json([
+                "message" => "Invalid parameter."
+            ], 400);
         }
 
-        return "記事一覧を取得しました（page={$page}）";
+        $articles = Article::paginate(2);
+
+        return response()->json($articles);
     }
 
     public function store()
